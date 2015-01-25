fee57726212e7dc3bee2424b424bae7d406efc96
Nabil Alami <nabil16@meet.mit.edu>
2015-01-25 19:50:15 +0200

    OOP

--- OOP.py
+++ OOP.py
@@ -1,11 +1,39 @@
 class Animal:
-	def __init__(self, name, age, color, size):
+	def __init__(self, name, age, color, size,):
 		self.name = name
 		self.age = age
 		self.color = color
 		self.size = size
-Dog = Animal("kookoo", 22, "Yellow", "tiny")
-print (Dog.name)
-print (Dog.age)
-print (Dog.color)
-print (Dog.size)
+		
+	def print_all(self):
+		print (self.name)
+		print (self.age)
+		print (self.color)
+		print (self.size)
+		
+	def sleep(self, hours, dreams):
+		print ("the animal", self.name, "is dreaming for", hours, "of", dreams)
+	
+	def sleep_2(self, hours, dreams):
+		print ("the animal", self.name, "is dreaming for", hours, "of", dreams)
+
+	def eat_2(self, food):
+		print ("the animal", self.name, "is eating", food)
+	
+	def eat(self, food):
+		print ("the animal", self.name, "is eating", food)
+
+
+Dog = Animal("Sohail", 22, "Yellow", "tiny",)
+Lion = Animal("Kookoo", 1, "burble", "small", )
+
+
+
+Dog.print_all()
+Dog.sleep_2(25,"cats")
+Dog.eat("pizza")
+Lion.print_all()
+Lion.eat_2("pasta")
+Lion.sleep(23,"python")
+
+
